9500a6592d67035257fce8f1bd0ee92c7a87488f
Yakir <got.yakir@gmail.com>
2022-01-29 20:06:36 +0100

    ci: move prisma to app folder
    #6

---
+++ src/app/prisma/prisma-client.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+import { addMultitenancy } from "app/prisma-connector";
+
+let prisma;
+
+export default function getDb() {
+  if (!prisma) {
+    prisma = new PrismaClient();
+
+    prisma.$use((params, next) => {
+      return addMultitenancy(params, next, prisma, "account_id");
+    });
+  }
+
+  return prisma;
+}
